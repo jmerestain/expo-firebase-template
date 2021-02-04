3d858dde0db6e9ed9c7d5aa792612dbb5eb0b3c7
camillejilliane <44397746+camillejilliane@users.noreply.github.com>
2021-02-04 23:35:43 +0800

    My Account Screen

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -28,7 +28,6 @@ const LoginScreen = ({navigation}) => {
             onPress={() => {
                 if(email != '' && password != '') {
                     loginUser(email, password, setMessage, navigation);
-                    navigation.navigate('Category')
                 } else {
                     setMessage('Credentials provided is not valid');
                 }
