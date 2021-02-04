623ede264073a90d085354bab96243a1f125737c
camillejilliane <44397746+camillejilliane@users.noreply.github.com>
2021-02-04 20:04:48 +0800

    Vendor Navigation, Favorites, Settings Screens

--- screens/VendorScreens/index.jsx
+++ screens/VendorScreens/index.jsx
@@ -3,6 +3,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import VendorScreen from './VendorScreen';
 import NewItemScreen from './NewItemScreen';
+import FavoritesScreen from './FavoritesScreen';
+import SettingsScreen from './SettingsScreen';
 
 const Vendor = createStackNavigator();
 const noHeader = {headerShown: false}
@@ -13,6 +15,10 @@ const VendorNavigator = () => {
             <Vendor.Screen name="VendorMain" component={VendorScreen} 
             options={noHeader} />
             <Vendor.Screen name="VendorNew" component={NewItemScreen} 
+            options={noHeader}  />
+            <Vendor.Screen name="Favorites" component={FavoritesScreen} 
+            options={noHeader}  />
+            <Vendor.Screen name="Settings" component={SettingsScreen} 
             options={noHeader}  />
         </Vendor.Navigator>        
     )
